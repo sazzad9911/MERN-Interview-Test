4872e73915a61cd6a0bb544f84e1a2734da18d09
Sazzad Hossain <57707331+sazzad9911@users.noreply.github.com>
2023-12-25 15:10:05 +0600

    dfd

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 app.use(cors());
-mongoConnection();
-app.use("/images",express.static(path.join(__dirname, "/images")))
+
+app.use("/images", express.static(path.join(__dirname, "/images")));
 app.use(express.static(path.join(__dirname, "frontend/build")));
-app.post("/api/v3/create-drawing",upload.single("thumbnail"),createDrawing);
-app.put("/api/v3/update-drawing",upload.single("thumbnail"),updateDrawing);
+app.post("/api/v3/create-drawing", upload.single("thumbnail"), createDrawing);
+app.put("/api/v3/update-drawing", upload.single("thumbnail"), updateDrawing);
 app.get("/api/v3/get-drawings", getDrawings);
 app.get("/api/v3/get-drawing-by/:id", getDrawingById);
 app.delete("/api/v3/delete-drawing/:id", deleteDrawing);
@@ -31,7 +31,8 @@ app.delete("/api/v3/delete-drawing/:id", deleteDrawing);
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend/build/index.html"));
 });
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+mongoConnection().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 });
